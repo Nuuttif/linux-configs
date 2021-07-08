daf59582f33f11a7a435534984bfe682fa02c822
Nuuttif <75477713+Nuuttif@users.noreply.github.com>
2021-07-08 22:07:23 +0300

    added picom

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-pacman -S xorg xorg-server xorg-xinit i3-gaps imagemagick nitrogen firefox alacritty
+pacman -S xorg xorg-server xorg-xinit i3-gaps imagemagick nitrogen firefox alacritty picom
 mv config ..
 mv fi ..
 mv .xinitrc ..
 mv .zshrc ..
 mv w ..
+mv picom.conf ..
 
 cd ..
 mv fi /usr/share/X11/xkb/symbols
@@ -14,6 +15,9 @@ mkdir Wallpapers
 mkdir .config
 mkdir .config/i3
 mv config .config/i3
+
+mkdir .config/picom
+mv picom.conf .config/picom
 
 git clone https://github.com/dylanaraps/wal
 mv wal/wal /usr/local/bin

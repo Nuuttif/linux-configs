0f1675aba4b7e692980d86d9f809d40dba9e40a3
Nuuttif <75477713+Nuuttif@users.noreply.github.com>
2021-07-08 20:58:46 +0300

    Fixex wallpaper script and added Wallpapers folder

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ mv w ..
 cd ..
 mv fi /usr/share/X11/xkb/symbols
 
+mkdir Wallpapers
 mkdir .config
 mkdir .config/i3
 mv config .config/i3
@@ -18,6 +19,7 @@ git clone https://github.com/dylanaraps/wal
 mv wal/wal /usr/local/bin
 rm -r wal
 
+chmod +x w
 mv w /usr/local/bin
 
 exit

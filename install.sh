b0adfa5b8231fefefc431af28d9e1fe3689a5eaf
Nuuttif <75477713+Nuuttif@users.noreply.github.com>
2021-07-10 17:37:44 +0300

    removed wifi stuff

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pacman -S xorg xorg-server xorg-xinit i3-gaps imagemagick nitrogen firefox alacritty feh pcmanfm dhclient
+pacman -S xorg xorg-server xorg-xinit i3-gaps imagemagick nitrogen firefox alacritty feh pcmanfm
 mv config ..
 mv fi /usr/share/X11/xkb/symbols
 mv .xinitrc ..

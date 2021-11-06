d224ce0fc1d0066da32f6277462a2227233942cf
Nuuttif <75477713+Nuuttif@users.noreply.github.com>
2021-11-06 17:25:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-pacman -S xorg xorg-server xorg-xinit i3-gaps imagemagick nitrogen firefox alacritty feh pcmanfm alsa-utils asoundconf rofi
+pacman -S xorg xorg-server xorg-xinit i3-gaps imagemagick nitrogen firefox alacritty feh pcmanfm alsa-utils asoundconf rofi i3status-rust
 
 cp -r i3 ..
 cp -r alacritty ..
 cp -r picom ..
 cp -r rofi ..
 cp -r Wallpapers ..
+cp -r i3status-rust ..
 
 cp -r ubuntu /usr/share/fonts
 
@@ -22,6 +23,7 @@ cp -r i3 .config
 cp -r picom .config
 cp -r alacritty .config
 cp -r rofi .config
+cp -r i3status-rust .config
 
 git clone https://github.com/dylanaraps/wal
 mv wal/wal /usr/local/bin
